183faf7f959e3d4cc3b73beaedc06221fa685ba7
Ema Sava <savaema97@gmail.com>
2023-06-28 15:41:54 +0300

    implement searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,16 +1,33 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FormEvent, useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={"20px"}
-        placeholder="Search games..."
-        variant={"filled"}
-      />
-    </InputGroup>
+    <form
+      onSubmit={(event: FormEvent) => {
+        event.preventDefault();
+        if (ref.current !== null) {
+          onSearch(ref.current.value);
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          borderRadius={"20px"}
+          placeholder="Search games..."
+          variant={"filled"}
+          ref={ref}
+        />
+      </InputGroup>
+    </form>
   );
 };
 
